85425231a05a8b1828a24b9748dc93ecde0f140c
VAASIFAHMED5445 <aasifahmed.v2025@vitstudent.ac.in>
2026-05-17 07:07:02 +0530

    Built OCR upload system

--- backend/app.py
+++ backend/app.py
@@ -1,1 +1,39 @@
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+import pytesseract
+import cv2
+import numpy as np
 
+app = Flask(__name__)
+CORS(app)
+
+# Tesseract path
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+@app.route('/')
+def home():
+    return "HandSolveAi Backend Running"
+
+@app.route('/upload', methods=['POST'])
+def upload_image():
+    try:
+        file = request.files['image']
+
+        image_bytes = np.frombuffer(file.read(), np.uint8)
+        img = cv2.imdecode(image_bytes, cv2.IMREAD_COLOR)
+
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+        text = pytesseract.image_to_string(gray)
+
+        return jsonify({
+            "detected_text": text
+        })
+
+    except Exception as e:
+        return jsonify({
+            "error": str(e)
+        })
+
+if __name__ == '__main__':
+    app.run(debug=True)
